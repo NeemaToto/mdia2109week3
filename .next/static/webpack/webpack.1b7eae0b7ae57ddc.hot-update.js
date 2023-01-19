b8cd71304ec8974c4c69099c5a69fe8762290964
Neema Totonchi <ntotonchi1@se14l0120-13.ad.bcit.ca>
2023-01-19 13:05:33 -0800

    add styling

--- .next/static/webpack/webpack.1b7eae0b7ae57ddc.hot-update.js
+++ .next/static/webpack/webpack.1b7eae0b7ae57ddc.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "dfb4009750677881"; }
+/******/ 	__webpack_require__.h = function() { return "85d2ab09770dad15"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
