301bea1b5edd38337e14275faacf2ad08060e141
NeemaToto <neematotonchi@gmail.com>
2023-01-19 20:30:41 -0800

    fix text color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c1434a9c9a01ce7"; }
+/******/ 		__webpack_require__.h = function() { return "9a58a9decbc4d7d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
