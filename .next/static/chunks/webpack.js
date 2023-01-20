68110e73ffdf45d78168a9260f0f0b8896c92a0b
NeemaToto <neematotonchi@gmail.com>
2023-01-19 20:25:11 -0800

    finished styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "077691ae217d86ae"; }
+/******/ 		__webpack_require__.h = function() { return "5c1434a9c9a01ce7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
